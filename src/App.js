e84fdd037c5299d06eeeeb6085cdc5485693b4b2
binarypsilocybin <53083287+binarypsilocybin@users.noreply.github.com>
2022-12-28 11:50:02 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,5 +2,11 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
-  return <div>hello react</div>;
+  return (
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className="temperature-display">10ºC</div>
+      </div>
+    </div>
+  );
 }
